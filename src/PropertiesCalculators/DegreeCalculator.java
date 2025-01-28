d983378c2cf90312b7b896511dc555166eeb0b5d
jonas <jonaslechner23456789@gmail.com>
2025-01-28 17:21:55 +0100

    updated Degree Calculation classes

--- src/PropertiesCalculators/DegreeCalculator.java
+++ src/PropertiesCalculators/DegreeCalculator.java
@@ -1,70 +1,23 @@
 package PropertiesCalculators;
 
-import PropertiesCalculators.Helper.Edge;
 import PropertiesCalculators.Helper.Graph;
 
 public class DegreeCalculator {
-    public static void maxInDegree(Graph graph, String graphName) {
-        int maxInDegree = -1;
-        int lastMaxNode = -1;
-
-        for (int i = 1; i <= graph.getMaxNode(); i++) {
-            int inDegree = 0;
-            for (Edge edge: graph.getEdges()) {
-                if (edge.getNode2() == i)
-                    ++inDegree;
-            }
-            if (inDegree > maxInDegree) {
-                maxInDegree = inDegree;
-                lastMaxNode = i;
-            }
-
-        }
-
-        System.out.println("Max inDegree of " + graphName + " = " + maxInDegree + ", at Node " + lastMaxNode);
-
-    }
-
-    public static void maxOutDegree(Graph graph, String graphName) {
-        int maxOutDegree = -1;
-        int lastMaxNode = -1;
-
-        for (int i = 1; i <= graph.getMaxNode(); i++) {
-            int outDegree = 0;
-            for (Edge edge: graph.getEdges()) {
-                if (edge.getNode1() == i)
-                    ++outDegree;
-            }
-            if (outDegree > maxOutDegree) {
-                maxOutDegree = outDegree;
-                lastMaxNode = i;
-            }
-
-        }
-
-        System.out.println("Max outDegree of " + graphName + " = " + maxOutDegree + ", at Node " + lastMaxNode);
-
-    }
-
-    public static void averageInAndOutDegree(Graph graph, String graphName) {
-
-
-        System.out.println("Average out- or inDegree of " + graphName + " = " + ((double)graph.getNumEdges())/graph.getMaxNode());
-
-    }
-
     public static void main(String[] args) {
-        String graphName = "BMLP_5_2_3_1.dgf";
+        String graphName = "IndRNN_3_100_100_100_100_1.dgf";
+
         String testFolder = "testing/";
         String thesisFolderFF = "thesis/feedForward/";
         String thesisFolderRecurrent = "thesis/recurrent/";
-        String graphPath = "graphs/" + thesisFolderFF + graphName;
+        //take the folder where the graph is stored
+        String graphPath = "graphs/" + thesisFolderRecurrent + graphName;
         Graph graph = new Graph(graphPath);
 
-        maxInDegree(graph, graphName);
-        maxOutDegree(graph, graphName);
-        averageInAndOutDegree(graph, graphName);
-
+        graph.calculateMaxInDegree();
+        graph.calculateMaxOutDegree();
+        graph.calculateMaxTotalDegree();
+        graph.calculateAverageInOutDegree();
+        graph.calculateAverageTotalDegree();
     }
 
 }
--- src/PropertiesCalculators/Helper/Graph.java
+++ src/PropertiesCalculators/Helper/Graph.java
@@ -9,9 +9,39 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Graph {
-    int maxNode;
-    int numEdges;
-    List<Edge> edges = new ArrayList<>();
+    private int maxNode;
+    private int numEdges;
+    private List<Edge> edges = new ArrayList<>();
+    private String graphName;
+
+    public Graph(String input) {
+        parseName(input);
+        try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                if (line.startsWith("p")) {
+                    parseMetaInformation(line);
+                } else
+                // Ignore comment lines
+                if (line.startsWith("c")) {
+                    continue;
+                } else
+                //edge
+                if (line.startsWith("e")) {
+                    parseEdge(line);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void parseName(String path) {
+        String normalizedPath = path.replace("\\", "/");
+        graphName = normalizedPath.substring(normalizedPath.lastIndexOf("/") + 1);
+    }
 
     private void parseMetaInformation(String line) {
         Pattern pattern = Pattern.compile("\\d+");
@@ -51,29 +81,6 @@ public class Graph {
             throw new RuntimeException("to many or to little matches for numEdges");
     }
 
-    public Graph(String input) {
-        try (BufferedReader br = new BufferedReader(new FileReader(input))) {
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                line = line.trim();
-                if (line.startsWith("p")) {
-                    parseMetaInformation(line);
-                } else
-                // Ignore comment lines
-                if (line.startsWith("c")) {
-                    continue;
-                } else
-                //edge
-                if (line.startsWith("e")) {
-                    parseEdge(line);
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
-        }
-    }
-
     public Graph() {
         throw new RuntimeException("can't use empty constructor");
     }
@@ -101,4 +108,91 @@ public class Graph {
     public void setEdges(List<Edge> edges) {
         this.edges = edges;
     }
+
+    public void calculateMaxInDegree () {
+        int maxInDegree = -1;
+        int lastMaxNode = -1;
+
+        for (int i = 1; i <= maxNode; i++) {
+            int inDegree = 0;
+            for (Edge edge: edges) {
+                if (edge.getNode2() == i)
+                    ++inDegree;
+            }
+            if (inDegree > maxInDegree) {
+                maxInDegree = inDegree;
+                lastMaxNode = i;
+            }
+        }
+        System.out.println("Max in-degree of " + graphName + " = " + maxInDegree + ", at Node " + lastMaxNode);
+
+    }
+
+    public void calculateMaxOutDegree () {
+        int maxOutDegree = -1;
+        int lastMaxNode = -1;
+
+        for (int i = 1; i <= maxNode; i++) {
+            int outDegree = 0;
+            for (Edge edge: edges) {
+                if (edge.getNode1() == i)
+                    ++outDegree;
+            }
+            if (outDegree > maxOutDegree) {
+                maxOutDegree = outDegree;
+                lastMaxNode = i;
+            }
+        }
+
+        System.out.println("Max out-degree of " + graphName + " = " + maxOutDegree + ", at Node " + lastMaxNode);
+    }
+
+    public void calculateMaxTotalDegree () {
+        int maxTotalDegree = -1;
+        int lastMaxNode = -1;
+
+        for (int i = 1; i <= maxNode; i++) {
+            int totalDegree = 0;
+            for (Edge edge: edges) {
+                if (edge.getNode1() == i || edge.getNode2() == i)
+                    ++totalDegree;
+            }
+            if (totalDegree > maxTotalDegree) {
+                maxTotalDegree = totalDegree;
+                lastMaxNode = i;
+            }
+        }
+        System.out.println("Max total degree of " + graphName + " = " + maxTotalDegree + ", at Node " + lastMaxNode);
+    }
+
+    public void calculateAverageInOutDegree () {
+        double totalDegreeGraph = 0;
+
+        for (int i = 1; i <= maxNode; i++) {
+            int inDegree = 0;
+            for (Edge edge: edges) {
+                if (edge.getNode2() == i) //doesn't matter if we take node 1 or node 2 result for average in- and out-degree is the same
+                    ++inDegree;
+            }
+            totalDegreeGraph += inDegree;
+        }
+        System.out.println("Average in- and out-degree of " + graphName + " = " + totalDegreeGraph/maxNode);
+    }
+
+    public void calculateAverageTotalDegree () {
+        double totalDegreeGraph = 0;
+
+        for (int i = 1; i <= maxNode; i++) {
+            int inDegree = 0;
+            for (Edge edge: edges) {
+                if (edge.getNode1() == i || edge.getNode2() == i)
+                    ++inDegree;
+            }
+            totalDegreeGraph += inDegree;
+
+        }
+
+        System.out.println("Average total degree of " + graphName + " = " + totalDegreeGraph/maxNode);
+    }
+
 }
